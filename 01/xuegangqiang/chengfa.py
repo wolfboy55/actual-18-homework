a452ed0bd36c4d18b5e417edab1f082ecd2709e6
xuegqcto(薛刚强) <xuegqcto@aliyun.com>
2018-03-18 23:06:21 +0800

    Add Xuegangqiang

---
+++ 01/xuegangqiang/chengfa.py
@@ -0,0 +1,18 @@
+#!/bin/bin/env python3
+# -*- coding: utf-8 -*-
+# __auhtor__: will_xue
+# Date: 2018-03-18
+# Email: xuegqcto@aliyun.com
+
+# 打印乘法口诀表
+
+# 定义外循环值
+i = 1
+while i <= 9:
+    #定义内循环，保持每次 j 初值为1
+    j = 1
+    while j <= i:
+        print(str(j) + '*' + str(i) + '=' + str(j * i), end=' ')
+        j = j + 1
+    print()
+    i = i + 1
