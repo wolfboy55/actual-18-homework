cfd5474dfa51ea5cb8db148f32efad80b4483ce2
root <root@VM_0_5_centos.(none)>
2018-03-19 02:23:16 +0800

    zzj's commit

--- 01/zhengzhijiang/99.py
+++ 01/zhengzhijiang/99.py
@@ -4,5 +4,6 @@
 for i in range(1,10):
     for j in range(1,10):
         if i >= j:
-            print '%s x %s = %s\t' %(i,j,i*j),
-    print '\n'
+            print ('%s x %s = %s\t' %(i,j,i*j),end='')
+            #print '%s x %s = %s\t' %(i,j,i*j), python2
+    print ('\n')
